f1d69c81cb714d5c299f43910dbf529e37fb4e38
ernestorangel <ernestojrcjunior@gmail.com>
2023-03-21 11:57:14 -0300

    working boilerplate

--- pages/index.vue
+++ pages/index.vue
@@ -18,15 +18,14 @@
 
 <script>
 import ProductCard from '@/components/ProductCard';
+import products from '@/mocks/products.json';
+
 export default {
   components: { ProductCard },
-  data() {
-    return {
-      products: [],
-    };
-  },
-  async created() {
-    this.products = (await this.$axios.get('/api/products')).data.products;
+  computed: {
+    products() {
+      return products;
+    },
   },
 };
 </script>
